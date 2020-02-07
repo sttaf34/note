967a310b9952c9bcffafd020201900a4b03e87ec
Shinichi Kawamura <sttaf34@gmail.com>
2020-02-07 14:01:48 +0900

    コメント修正

--- command/other/local_server.sh
+++ command/other/local_server.sh
@@ -1,9 +1,10 @@
 
-# 今いるところでローカルサーバ起動
-# index.htmlがない場合、ファイル一覧表示してくれる
 # npm install -g serve
-# これはグローバルに入れてしまって良いかもかな？
-cd ~/Projects/Documents/HTMLCSS && serve
+# これはグローバルに入れてしまう
 
-# index.htmlがない場合、404になるのでちょい不便
-cd ~/Projects/Documents/HTMLCSS && php -S localhost:8080
+# ローカルサーバ起動
+# index.htmlがない場合、ファイル一覧表示してくれる
+cd ~/Projects/note/htmlcss && serve
+
+# PHPでもできるが、index.htmlがない場合、404になるのでちょい不便
+cd ~/Projects/note/htmlcss && php -S localhost:8080
