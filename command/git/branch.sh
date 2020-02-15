fdeb8e28120b591d69cf87ce0db46ee8d0dbb769
Shinichi Kawamura <sttaf34@gmail.com>
2020-02-15 18:58:51 +0900

    git branch のメモ追加

--- command/git/branch.sh
+++ command/git/branch.sh
@@ -7,3 +7,9 @@ git branch -v
 
 # -vv -> 表示にコミットハッシュとブランチ名が含まれるようになる
 git branch -vv
+
+# -m -> ブランチ名変更、今いるブランチ名を変更
+git branch -m NEW_BRANCH_NAME
+
+# これでも可
+git branch -m OLD_BRANCH_NAME NEW_BRANCH_NAME
