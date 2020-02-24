6df9204286e2d73c463a593a75fc167e377a05d6
Shinichi Kawamura <sttaf34@gmail.com>
2020-02-24 22:56:33 +0900

    Mac の設定変更メモ追加

--- command/mac/mac_setting.sh
+++ command/mac/mac_setting.sh
@@ -7,3 +7,12 @@ defaults write com.apple.dock persistent-others -array-add '{tile-data={}; tile-
 
 # 再起動すると有効になる
 killall Dock
+
+# スクショの保存場所変更
+defaults write com.apple.screencapture location ~/Workspace/
+
+# スクショのファイル名変更
+defaults write com.apple.screencapture name ""
+
+# スクショ設定の有効化
+killall SystemUIServer
