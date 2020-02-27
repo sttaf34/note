f6609ada5f852f7715c3411d9660e943e5e1ae2a
Shinichi Kawamura <sttaf34@gmail.com>
2020-02-27 20:43:37 +0900

    chmod, env, shebang のメモ追加

--- command/basic/chmod_id.sh
+++ command/basic/chmod_id.sh
@@ -1,5 +1,7 @@
 
-# xが1、wが2、rが4
+# x が 1 -> 実行権限
+# w が 2 -> 書き込み権限
+# r が 4 -> 読み込み権限
 # 左から、所有ユーザ・所有グループ・その他の順番
 chmod 755 test.sh
 
@@ -7,13 +9,19 @@ chmod 755 test.sh
 # 644 -> -rw-r--r--
 # 755 -> drwxr-xr-x
 
-# ログインしているユーザ名・そのユーザのプライマリグループ・所属中グループの表示
-id
--> uid=501(kawa) gid=20(staff) groups=20(staff), .....
+# 実行権限付与系
+chmod u+x test.sh # ユーザに実行権限付与
+chmod g+x test.sh # グループに実行権限付与
+chmod o+x test.sh # その他に実行権限付与
+chmod +x test.sh  # 全体に実行権限付与
+chmod -x test.sh  # 全体から実行権限削除
 
-# ユ グ 他    ユ所  グ所
-# ｜ ル       ｜有  ル有
-# ザ ｜       ザ    ｜
+# ログインしているユーザ名・そのユーザのプライマリグループ・所属中グループの表示
+id # uid=501(kawa) gid=20(staff) groups=20(staff), .....
+
+# ユ グ そ    ユ所  グ所
+# ｜ ル の    ｜有  ル有
+# ザ ｜ 他    ザ    ｜
 #    プ             プ
 -rw-r--r--  1 kawa  staff  752 Apr  7 04:23 040603041396721048
 drwxr-xr-x  5 kawa  staff  170 Apr  6 10:40 MyWork
