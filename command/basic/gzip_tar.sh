1cc23de4dea74c679d703d8f261d74feaf293b22
Shinichi Kawamura <sttaf34@gmail.com>
2020-03-01 20:44:30 +0900

    tar コマンドのメモ増強

--- command/basic/gzip_tar.sh
+++ command/basic/gzip_tar.sh
@@ -1,3 +1,8 @@
+
+#
+# gzip
+#
+
 # 現状を確認
 cd ~/Projects/note/command/git
 ls -l gitconfig.sh
@@ -11,11 +16,19 @@ ls -l gitconfig.sh.gz
 # 解凍して gitconfig.sh に戻す
 gunzip gitconfig.sh.gz
 
-# Texts ディレクトリをアーカイブ
+#
+# tar
+#
+
+# text ディレクトリをアーカイブ
 # c => アーカイブを作成（一つのファイルにまとめる的な意味かと）
 # f => アーカイブファイル名の指定
+# v => 詳細表示
 cd ~/Projects/note
-tar cf text.tar text
+tar cfv text.tar text
+
+# アーカイブを作成し、アーカイブ対象は削除する
+tar cfv text.tar text --remove-files
 
 # 中身のファイル一覧の確認
 # t => 一覧を表示
@@ -24,5 +37,17 @@ tar tf text.tar
 # x => アーカイブから展開する
 tar xf text.tar
 
-# アーカイブを作成し、アーカイブ対象は削除する
-tar cf text.tar text --remove-files
+#
+# tar.gz
+#
+
+# text ディレクトリを text.tar.gz に圧縮
+# z -> gzip の指定
+cd ~/Projects/note
+tar cfvz text.tar.gz text
+
+# text.tar.gz の中身を確認
+tar tfvz text.tar.gz
+
+# text.tar.gz を展開する
+tar xfvz text.tar.gz
