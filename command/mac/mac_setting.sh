ebd3f5a6dbec163075b998ff1bd89e8890d3d8a3
Shinichi Kawamura <sttaf34@gmail.com>
2020-03-12 01:31:01 +0900

    Mac の設定メモの追加

--- command/mac/mac_setting.sh
+++ command/mac/mac_setting.sh
@@ -5,8 +5,10 @@ defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-ty
 # Dockのファイル側にスペース追加
 defaults write com.apple.dock persistent-others -array-add '{tile-data={}; tile-type="spacer-tile";}'
 
-# 再起動すると有効になる
+# 再起動で有効化
 killall Dock
+
+
 
 # スクショの保存場所変更
 defaults write com.apple.screencapture location ~/Workspace/
@@ -17,6 +19,15 @@ defaults write com.apple.screencapture name ""
 # スクショ設定の有効化
 killall SystemUIServer
 
+
+
 # .DS_Store ファイルを作らない or 作る
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 defaults write com.apple.desktopservices DSDontWriteNetworkStores false
+
+# Finder のタイトルバーに絶対パスの表示
+defaults write com.apple.finder _FXShowPosixPathInTitle -boolean true
+defaults write com.apple.finder _FXShowPosixPathInTitle -boolean true
+
+# 再起動で有効化
+killall Finder
