307686915f59d58c8657a426b48e6d73be7fed1f
Shinichi Kawamura <sttaf34@gmail.com>
2020-03-13 23:05:10 +0900

    MySQL のメモ編集

--- mysql/transaction/6-secondary-index-where-and.sql
+++ mysql/transaction/6-secondary-index-where-and.sql
@@ -11,50 +11,48 @@ SET GLOBAL innodb_status_output_locks = ON;
 SELECT @@innodb_status_output;
 SELECT @@innodb_status_output_locks;
 
-DROP TABLE IF EXISTS orders;
-CREATE TABLE orders (
-  id INT PRIMARY KEY,
+DROP TABLE IF EXISTS orders_single;
+CREATE TABLE orders_single (
+  id INT PRIMARY KEY AUTO_INCREMENT,
   user_id INT NOT NULL,
-  group_id INT NOT NULL,
+  product_id INT NOT NULL,
   INDEX index_user_id(user_id),
-  INDEX index_group_id(group_id)
+  INDEX index_product_id(product_id)
 );
 INSERT INTO
-  orders (id, user_id, group_id)
+  orders_single (user_id, product_id)
 VALUES
-  (1, 35), (2, 42);
-SELECT * FROM orders;
+  (1, 35), (1, 42), (1, 56),
+  (2, 28), (2, 35), (2, 56), (2, 77), (2, 84),
+  (3, 28), (3, 42), (3, 77), (3, 84),
+  (4, 56), (4, 77), (4, 84), (4, 100);
 
-PAGER awk '/Trx id counter/, /FILE I\/O/';
+DROP TABLE IF EXISTS orders_multi;
+CREATE TABLE orders_multi (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  product_id INT NOT NULL,
+  INDEX index_user_id_product_id(user_id, product_id)
+);
+INSERT INTO
+  orders_multi (user_id, product_id)
+VALUES
+  (1, 35), (1, 42), (1, 56),
+  (2, 28), (2, 35), (2, 56), (2, 77), (2, 84),
+  (3, 28), (3, 42), (3, 77), (3, 84),
+  (4, 56), (4, 77), (4, 84), (4, 100);
 
 --
--- デッドロック
+-- 2つのカラムを検索条件にして検索・複合インデックスではない場合
 --
 
--- A                                                        -- B
--- id = 1, id = 2 の group_id を入れ替える処理を試みる
 BEGIN;
-SELECT * FROM members WHERE id = 1 FOR UPDATE;
-                                                            BEGIN;
-                                                            SELECT * FROM members WHERE id = 2 FOR UPDATE;
+SELECT * FROM orders_single WHERE user_id = 1 AND product_id = 42 FOR UPDATE;
 SHOW ENGINE INNODB STATUS\G;
--- RECORD LOCKS space id 46 page no 3 n bits 72 index PRIMARY of table `sample`.`members` trx id 2766 lock_mode X locks rec but not gap
--- Record lock, heap no 3 PHYSICAL RECORD: n_fields 4; compact format; info bits 0
---  0: len 4; hex 80000002; asc     ;;
---  1: len 6; hex 000000000ac5; asc       ;;
---  2: len 7; hex bd000001b1011c; asc        ;;
---  3: len 4; hex 8000002a; asc    *;;
--- 80000002 => WHERE id = 2 に該当、レコードロック＠クラスタインデックス
---
--- RECORD LOCKS space id 46 page no 3 n bits 72 index PRIMARY of table `sample`.`members` trx id 2765 lock_mode X locks rec but not gap
--- Record lock, heap no 2 PHYSICAL RECORD: n_fields 4; compact format; info bits 0
---  0: len 4; hex 80000001; asc     ;;
---  1: len 6; hex 000000000ac5; asc       ;;
---  2: len 7; hex bd000001b10110; asc        ;;
---  3: len 4; hex 80000023; asc    #;;
--- 80000001 => WHERE id = 1 に該当、レコードロック＠クラスタインデックス
 
-SELECT * FROM members WHERE id = 2 FOR UPDATE;
--- 先行して B でレコードロックされているので時が止まる
-                                                            SELECT * FROM members WHERE id = 1 FOR UPDATE;
-                                                            -- デッドロック検出される
+--
+-- 2つのカラムを検索条件にして検索・複合インデックスな場合
+--
+BEGIN;
+SELECT * FROM orders_multi WHERE user_id = 1 AND product_id = 42 FOR UPDATE;
+SHOW ENGINE INNODB STATUS\G;
