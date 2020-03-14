1cb6bb659c1424d8c92ebc53bc01d07b4a9535fa
Shinichi Kawamura <sttaf34@gmail.com>
2020-03-14 19:31:31 +0900

    複合インデックス時のロックのメモ追加

--- mysql/transaction/6-secondary-index-where-and.sql
+++ mysql/transaction/6-secondary-index-where-and.sql
@@ -42,6 +42,8 @@ VALUES
   (3, 28), (3, 42), (3, 77), (3, 84),
   (4, 56), (4, 77), (4, 84), (4, 100);
 
+PAGER awk '/Trx id counter/, /FILE I\/O/';
+
 --
 -- 2つのカラムを検索条件にして検索・複合インデックスではない場合
 --
@@ -49,6 +51,7 @@ VALUES
 BEGIN;
 SELECT * FROM orders_single WHERE user_id = 1 AND product_id = 42 FOR UPDATE;
 SHOW ENGINE INNODB STATUS\G;
+-- 説明を書くのが面倒なくらいに、いろんな範囲がロックされる
 
 --
 -- 2つのカラムを検索条件にして検索・複合インデックスな場合
@@ -56,3 +59,28 @@ SHOW ENGINE INNODB STATUS\G;
 BEGIN;
 SELECT * FROM orders_multi WHERE user_id = 1 AND product_id = 42 FOR UPDATE;
 SHOW ENGINE INNODB STATUS\G;
+-- RECORD LOCKS space id 50 page no 4 n bits 88 index index_user_id_product_id of table `sample`.`orders_multi` trx id 2816 lock_mode X
+-- Record lock, heap no 3 PHYSICAL RECORD: n_fields 3; compact format; info bits 0
+--  0: len 4; hex 80000001; asc     ;;
+--  1: len 4; hex 8000002a; asc    *;;
+--  2: len 4; hex 80000002; asc     ;;
+-- 8000002a => WHERE product_id = 42 に該当、ネクストキーロック＠セカンダリインデックス
+-- (1, 42) がレコードロックされ、手前がギャップロックされる
+-- INSERT されているデータ的に ((1, 35),(1, 42)] の区間ということになる
+--
+-- RECORD LOCKS space id 50 page no 3 n bits 88 index PRIMARY of table `sample`.`orders_multi` trx id 2816 lock_mode X locks rec but not gap
+-- Record lock, heap no 3 PHYSICAL RECORD: n_fields 5; compact format; info bits 0
+--  0: len 4; hex 80000002; asc     ;;
+--  1: len 6; hex 000000000af6; asc       ;;
+--  2: len 7; hex e2000001bc011c; asc        ;;
+--  3: len 4; hex 80000001; asc     ;;
+--  4: len 4; hex 8000002a; asc    *;;
+-- 80000002 => WHERE id = 2 に該当、レコードロック＠クラスタインデックス
+
+-- RECORD LOCKS space id 50 page no 4 n bits 88 index index_user_id_product_id of table `sample`.`orders_multi` trx id 2816 lock_mode X locks gap before rec
+-- Record lock, heap no 4 PHYSICAL RECORD: n_fields 3; compact format; info bits 0
+--  0: len 4; hex 80000001; asc     ;;
+--  1: len 4; hex 80000038; asc    8;;
+--  2: len 4; hex 80000003; asc     ;;
+-- 80000038 => WHERE product_id = 56 に該当、ギャップロック＠セカンダリインデックス
+-- INSERT されているデータ的に ((1, 42),(1, 56)) の区間ということになる
