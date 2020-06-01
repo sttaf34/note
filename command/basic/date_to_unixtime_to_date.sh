5f0d09af18018ed2d21d7ffb0109c81e27230d28
Shinichi Kawamura <sttaf34@gmail.com>
2020-06-01 22:27:03 +0900

    date コマンドのメモ変更

--- command/basic/date_to_unixtime_to_date.sh
+++ command/basic/date_to_unixtime_to_date.sh
@@ -1,13 +1,30 @@
+#
+# Ubuntu
+#
 
-# unixtime to datetime
+# unixtime を渡すと「Tue Feb 11 10:00:00 UTC 2020」なフォーマットで出力
+#
 date -d @1581415200
 
-# フォーマット
+# unixtime をフォーマット指定して出力
 # 指定詞 -> https://ja.wikipedia.org/wiki/Date_(UNIX)
 date -d @1581415200 +"%Y-%m-%d %H:%M:%S"
 
-# datetime to unixtime
+# 日時を渡すと unixtime で出力
 date -d "2020-02-11 19:00:00" +%s
 
-# 現在時刻 to unixtime
+# 現在 unixtime を出力
 date +%s
+
+#
+# Mac
+#
+
+# 現在 unixtime を出力
+date +%s
+
+# 現在年月日を出力 2020-06-01
+date +%F
+
+# 現在年月日を出力 2020/06/01
+date +%x
