63419e7d96c4984cc26ba14a4c042e0472eaad2b
Shinichi Kawamura <sttaf34@gmail.com>
2020-06-15 21:28:21 +0900

    コマンドのメモ追加

--- command/basic/echo_sort.sh
+++ command/basic/echo_sort.sh
@@ -12,4 +12,8 @@ echo -e "123\n99\n85\n3" | sort -n
 echo -e "123\n99\n85\n3" | sort -nr
 
 # 環境変数を改行で分割して見やすく表示
+# Mac の Bash だとダメだった
 echo $PATH | sed -e "s/:/\n/g"
+
+# 環境変数を改行で分割して見やすく表示
+echo $PATH | tr '*' '\n'
