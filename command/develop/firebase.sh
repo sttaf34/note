f33a09bac6bd7d15897c7e995841e08b9cb839f8
Shinichi Kawamura <sttaf34@gmail.com>
2020-07-04 19:28:45 +0900

    firebase のメモ追加

---
+++ command/develop/firebase.sh
@@ -0,0 +1,13 @@
+# https://firebase.google.com/docs/cli?hl=ja
+
+# プロジェクトの一覧
+firebase projects:list
+
+# 既存のプロジェクトを Firebase 化するとき
+https://firebase.google.com/docs/cli?hl=ja#initialize_a_firebase_project
+
+# firebase.json の設定でデプロイ
+firebase deploy
+
+# デプロイの取り消し
+firebase hosting:disable
