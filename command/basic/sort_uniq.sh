e3e388228461b6859fe54e30f59320ca46243b72
Shinichi Kawamura <sttaf34@gmail.com>
2020-08-01 19:42:35 +0900

    sort のメモ追加

--- command/basic/sort_uniq.sh
+++ command/basic/sort_uniq.sh
@@ -20,3 +20,9 @@ history | head -20 | awk '{print $2}' | sort | uniq -c
 # さらに awk で条件指定で絞り込む
 # $1 が数えた数を指していて、それを検索条件にしてる
 history | head -20 | awk '{print $2}' | sort | uniq -c | awk '$1 > 1 {print $0}'
+
+# 文字数順にソートして、文字数とパスを表示する
+(cd ~/Projects/note/jquery && git ls-files) | awk '{print length(),$0}' | sort -n
+
+# 文字数順にソートして、パスを表示する、文字数は表示しない
+(cd ~/Projects/note/jquery && git ls-files) | awk '{print length(),$0}' | sort -n | awk '{print $2}'
