00bc9543acee8f5d70a6cabff9ea03ca05ca2fd3
Shinichi Kawamura <sttaf34@gmail.com>
2020-08-28 20:19:13 +0900

    git log のオプションのメモ追加

--- command/git/diff_log.sh
+++ command/git/diff_log.sh
@@ -1,4 +1,3 @@
-
 #
 # diff
 #
@@ -26,17 +25,21 @@ git diff --name-only XXXXXXXXXXXXXXXXXXXXX
 # 最小の情報での履歴表示
 git log --oneline
 
-# デフォルト
+# デフォルトだと、使うスペースの割に情報が少ない
 git log
 
 # 変更したファイルを含めた履歴表示
 git log --stat
 
-# 変更内容を含めた履歴表示
-git log -p
-
 # ファイルを限定しての履歴表示
 git log ~/Projects/note/command/basic/tail.sh
 
+# -p => 変更内容を表示
+git log -p
+
 # ファイルを限定して、変更内容も含めての履歴表示
 git log -p ~/Projects/note/command/basic/tail.sh
+
+# 日付で絞り込み
+git log --since="10 days ago" --until="8 days ago"
+git log --since="2019/12/01" --until="2019/12/31"
